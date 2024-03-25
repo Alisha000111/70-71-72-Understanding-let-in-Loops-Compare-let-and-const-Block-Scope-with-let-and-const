f514fec681c275c3d9f97379e0a6936f6d2a75a3
Student <160208713+Alisha000111@users.noreply.github.com>
2024-03-25 17:42:54 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,36 @@
+// 70
+// function printNumbersWithLet() {
+// for (let i = 1; i <= 5; i++) {
+// console.log(i); 
+// }
+// }
+// printNumbersWithLet();
+
+
+// 71
+//Using `let` for a variable that can be reassigned
+// let age = 20;
+// age = 30; 
+// console.log(age); 
+// const name = "Alisha";
+// try {
+// name = "tania"; 
+// } catch (error) {
+// console.log("Error: Can't reassign a `const`-declared variable."); 
+// }
+
+
+//72
+ let age = 30;
+ age = 40;
+ console.log(age);
+ const name = "ifra";
+ try {
+ name = "urossa";
+ } catch (error) {
+  console.log("Error: Can't reassign a `const`-declared variable."); 
+ }
+
+
+
+
